3884eab0ebbd1608ef45fedb79de321d3dfa4f4c
vito-filo <filomeno.vito@gmail.com>
2025-05-08 11:59:56 +0200

    feat: ConfigModule init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        CLIENT_ID: Joi.string()
+          .required()
+          .pattern(/^[a-zA-Z0-9_+]+$/)
+          .messages({
+            'string.pattern.base':
+              '"CLIENT_ID" fails to match the required pattern: /^[a-zA-Z0-9_+]+$/',
+          }),
+        CLIENT_SECRET: Joi.string()
+          .required()
+          .pattern(/^[a-zA-Z0-9_+]+$/)
+          .messages({
+            'string.pattern.base':
+              '"CLIENT_SECRET" fails to match the required pattern: /^[a-zA-Z0-9_+]+$/',
+          }),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
