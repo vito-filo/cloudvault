ab3ae76fed645341a55c9df584fe37b26529f2fd
vito-filo <filomeno.vito@gmail.com>
2025-05-09 11:15:17 +0200

    feat: add signup endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { SignupDto } from './dto/signup.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('login')
   signIn(@Body() loginDto: LoginDto) {
     return this.authService.authenticateWithCognito(loginDto);
+  }
+
+  @Post('signup')
+  signUp(@Body() signupDto: SignupDto) {
+    return this.authService.signUpWithCognito(signupDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,12 @@
 import { Injectable, HttpStatus, HttpException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { SignupDto } from './dto/signup.dto';
 import {
   CognitoIdentityProviderClient,
   InitiateAuthCommand,
+  SignUpCommand,
+  SignUpCommandOutput,
+  InitiateAuthCommandOutput,
 } from '@aws-sdk/client-cognito-identity-provider';
 import * as crypto from 'crypto';
 import { ConfigService } from '@nestjs/config';
@@ -30,7 +34,10 @@ export class AuthService {
       .update(username + clientId)
       .digest('base64');
   }
-  async authenticateWithCognito(loginDto: LoginDto) {
+
+  async authenticateWithCognito(
+    loginDto: LoginDto,
+  ): Promise<InitiateAuthCommandOutput> {
     try {
       const client = new CognitoIdentityProviderClient({
         region: 'eu-south-1',
@@ -50,10 +57,33 @@ export class AuthService {
           ),
         },
       });
-      return client.send(command);
+      return await client.send(command);
     } catch (error) {
       console.error('Error during authentication:', error);
       throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+    }
+  }
+
+  async signUpWithCognito(signupDto: SignupDto): Promise<SignUpCommandOutput> {
+    try {
+      const client = new CognitoIdentityProviderClient({
+        region: 'eu-south-1',
+      });
+
+      const signUpCommand = new SignUpCommand({
+        ClientId: this.CLIENT_ID,
+        SecretHash: this.generateSecretHash(
+          signupDto.email,
+          this.CLIENT_ID,
+          this.CLIENT_SECRET,
+        ),
+        Username: signupDto.email,
+        Password: signupDto.password,
+      });
+      return await client.send(signUpCommand);
+    } catch (error) {
+      console.error('Error during sign up:', error);
+      throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
     }
   }
 }
