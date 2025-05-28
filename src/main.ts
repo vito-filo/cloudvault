599ba041ca9f616bd4742eca39055b3281bd706f
vito-filo <filomeno.vito@gmail.com>
2025-05-28 13:49:04 +0200

    feat: add requests logging

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LoggingMiddleware } from './middleware/logging.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+  app.use(new LoggingMiddleware().use);
+
   app.enableCors({
     origin: process.env.ALLOWED_ORIGINS?.split(','),
     credentials: true,
