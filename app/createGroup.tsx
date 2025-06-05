8f1eceff176445e0b85346e43846d0ffbba4444b
vito-filo <filomeno.vito@gmail.com>
2025-06-05 12:16:11 +0200

    feat(group): add group members on create group

--- app/createGroup.tsx
+++ app/createGroup.tsx
@@ -1,10 +1,25 @@
 import { useUserData } from "@/context/authContext";
 import { useApi } from "@/hooks/useApi";
 import { formStyle } from "@/styles/createForm";
-import { GroupInput } from "@/types/group";
+import { GroupDetails, GroupInput } from "@/types/group";
+import AntDesign from "@expo/vector-icons/AntDesign";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import { useCallback, useEffect, useState } from "react";
-import { Alert, Pressable, Text, TextInput, View } from "react-native";
+import {
+  Alert,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { MultiSelect } from "react-native-element-dropdown";
+
+type UserData = {
+  email: string;
+  name?: string;
+  searchIndex?: string; // conposed of name - email for searching
+};
 
 export default function PasswordDetail() {
   const { groupIdParam, groupNameParam } = useLocalSearchParams<{
@@ -18,12 +33,36 @@ export default function PasswordDetail() {
   const [grouDescription, setGroupDescription] = useState<string | undefined>(
     ""
   );
+  const [searchData, setSearchData] = useState<UserData[]>([]);
+  const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
+
+  async function handleSearch(searchParam: string) {
+    try {
+      const response = await apiFetch<UserData[]>(
+        `/group/user/search?name=${searchParam}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      response.forEach((user) => {
+        user.searchIndex = `${user.name} - ${user.email}`;
+      });
+      setSearchData(response);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+      alert("Failed to fetch user data. Please try again later.");
+    }
+  }
 
   const fetchData = useCallback(async () => {
     if (!groupIdParam) return;
 
     try {
-      const response = await apiFetch<GroupInput>(
+      const response = await apiFetch<GroupDetails>(
         `/group?userId=${userData.id}&groupId=${groupIdParam}`,
         {
           headers: {
@@ -34,6 +73,18 @@ export default function PasswordDetail() {
       );
       setGroupName(response.name);
       setGroupDescription(response.description);
+
+      // set group members
+      const members = response.members.map((member) => member.user.email);
+      setSelectedUsers(members);
+
+      // Due to Multimodal internal behavior, we need to set searchData
+      const memberData = response.members.map((member) => ({
+        email: member.user.email,
+        name: member.user.name,
+        searchIndex: `${member.user.name} - ${member.user.email}`,
+      }));
+      setSearchData(memberData);
     } catch (err) {
       console.error("Failed to fetch group data:", err);
       Alert.alert(
@@ -67,6 +118,7 @@ export default function PasswordDetail() {
     const inputData: GroupInput = {
       name: groupName,
       description: grouDescription,
+      membersEmail: selectedUsers,
     };
 
     const method = groupIdParam ? "PATCH" : "POST";
@@ -100,7 +152,6 @@ export default function PasswordDetail() {
   return (
     <View style={{ padding: 20 }}>
       <Text style={{ fontWeight: "bold" }}>Group Name:</Text>
-      {/* <Text style={{ marginBottom: 20 }}>{data.serviceName}</Text> */}
       <TextInput
         style={formStyle.input}
         placeholder="Enter group name"
@@ -115,6 +166,36 @@ export default function PasswordDetail() {
         defaultValue={grouDescription}
         onChangeText={setGroupDescription}
       />
+
+      <Text style={{ fontWeight: "bold" }}>Group Members</Text>
+      <MultiSelect
+        style={formStyle.input}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        inputSearchStyle={styles.inputSearchStyle}
+        iconStyle={styles.iconStyle}
+        search
+        data={searchData}
+        labelField="searchIndex"
+        valueField="email"
+        placeholder={"Select group members"}
+        searchPlaceholder="Search..."
+        value={selectedUsers}
+        onChange={(item) => {
+          setSelectedUsers(item);
+        }}
+        onChangeText={handleSearch}
+        renderLeftIcon={() => (
+          <AntDesign
+            style={styles.icon}
+            color="black"
+            name="Safety"
+            size={20}
+          />
+        )}
+        selectedStyle={styles.selectedStyle}
+      />
+
       <Pressable style={formStyle.button} onPress={handlePress}>
         <Text style={formStyle.buttonText}>
           {groupIdParam ? "UPDATE" : "CREATE"}
@@ -123,3 +204,34 @@ export default function PasswordDetail() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { marginVertical: 15 },
+  dropdown: {
+    height: 50,
+    paddingHorizontal: 8,
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 7,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 14,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  selectedStyle: {
+    borderRadius: 12,
+  },
+});
--- types/group.ts
+++ types/group.ts
@@ -9,4 +9,14 @@ export type GroupList = {
 export type GroupInput = {
   name: string;
   description?: string;
+  membersEmail?: string[];
+};
+
+export type GroupDetails = {
+  id: string;
+  description?: string;
+  name: string;
+  members: { user: { name: string; email: string }; isAdmin: boolean }[];
+  updatedAt: Date;
+  createdAt: Date;
 };
